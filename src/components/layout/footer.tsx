42df6e64d3bce5006ffa83a6ae0fe3cf54c0bab1
bosshamster69 <c4110599@gmail.com>
2025-07-11 09:46:53 +0900

    💄 Style: 푸터 컴포넌트 피그마 시안에 맞게 간격 조정 푸터 컴포넌트 피그마 시안에 맞게 간격 조정 Close #9

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -24,16 +24,19 @@ export default function Footer() {
         <span className={footer_styles.divider}>|</span>
         <a href="#">불법 사이트 신고 및 제보</a>
       </div>
-      <div className={footer_styles.footer__company}>
         <div className={footer_styles.footer__company_name}><strong>Sophie&apos;s Warehouse</strong></div>
+        <div className={footer_styles.footer__company}>
         <p>
           대표자 김준오 ｜ 사업자등록번호 123-45-6789 <br />
           서울 금천구 가산디지털단지 가산로 1길 78, 10층 <br />
           통신판매업신고번호 제2023-서울금천-0918호 <br />
           개인정보 보호책임자 김준오 <br />
         </p>
-        <p>출처: http://www.kbrising.com/product/detail/12345/</p>
-      </div>
+        </div>
+        <div className={footer_styles.footer__resource}>
+          <p>출처: http://www.kbrising.com/product/detail/12345/</p>
+        </div>
+     
     </footer>
   );
 }
