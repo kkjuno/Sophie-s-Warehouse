5e6538d2c2e947b822669fe1f55beefa6de1a45b
bosshamster69 <c4110599@gmail.com>
2025-07-22 10:31:37 +0900

    💄 Style: 반응형 구현을 위한 중단점 수정 및 파일 이름 변경

--- src/components/layout/footerNavBar.tsx
+++ src/components/layout/footerNavBar.tsx
@@ -1,6 +1,6 @@
-import "@/styles/globals.css";
-import footer_nav_bar_styles from "@/styles/components/footerNavBar.module.css";
-import Link from "next/link";
+import '@/styles/globals.css';
+import footer_nav_bar_styles from '@/styles/components/footerNavBar.module.css';
+import Link from 'next/link';
 
 export default function FooterNav() {
   return (
@@ -8,31 +8,31 @@ export default function FooterNav() {
       <nav className={footer_nav_bar_styles.nav}>
         <Link href="#" className={footer_nav_bar_styles.navItem}>
           <div className={footer_nav_bar_styles.icon}>
-            <img src="/icons/장바구니 흰색 아이콘.svg" width={33} height={33} alt="장바구니" />
+            <img src="/icons/cart-white-icon.svg" width={33} height={33} alt="장바구니" />
           </div>
           <span className={footer_nav_bar_styles.label}>장바구니</span>
         </Link>
         <Link href="#" className={footer_nav_bar_styles.navItem}>
           <div className={footer_nav_bar_styles.icon}>
-            <img src="/icons/스템프 흰색 아이콘.svg" width={33} height={33} alt="스템프" />
+            <img src="/icons/stamp-white-icon.svg" width={33} height={33} alt="스템프" />
           </div>
           <span className={footer_nav_bar_styles.label}>스템프</span>
         </Link>
         <Link href="#" className={footer_nav_bar_styles.navItem}>
           <div className={footer_nav_bar_styles.icon}>
-            <img src="/icons/홈 흰색 아이콘.svg" width={33} height={33} alt="홈" />
+            <img src="/icons/home-white-icon.svg" width={33} height={33} alt="홈" />
           </div>
           <span className={footer_nav_bar_styles.label}>홈</span>
         </Link>
         <Link href="#" className={footer_nav_bar_styles.navItem}>
           <div className={footer_nav_bar_styles.icon}>
-            <img src="/icons/마이페이지 흰색 아이콘.svg" width={33} height={33} alt="마이페이지" />
+            <img src="/icons/mypage-white-icon.svg" width={33} height={33} alt="마이페이지" />
           </div>
           <span className={footer_nav_bar_styles.label}>마이페이지</span>
         </Link>
         <Link href="#" className={footer_nav_bar_styles.navItem}>
           <div className={footer_nav_bar_styles.icon}>
-            <img src="/icons/최근본상품 흰색 아이콘.svg" width={33} height={33} alt="최근본상품" />
+            <img src="/icons/recent-white-icon.svg" width={33} height={33} alt="최근본상품" />
           </div>
           <span className={footer_nav_bar_styles.label}>최근본상품</span>
         </Link>
