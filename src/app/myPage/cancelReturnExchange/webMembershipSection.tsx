aa4a8982ce5b049eac1a373b05deb11689233b6c
yuha2 <dmswl980208@naver.com>
2025-07-25 14:53:52 +0900

    ♻️ refactor : webMembershipSection 페이지 수정

--- src/app/myPage/cancelReturnExchange/webMembershipSection.tsx
+++ src/app/myPage/cancelReturnExchange/webMembershipSection.tsx
@@ -1,23 +1,33 @@
-// components/myPage/cancelReturnExchange/WebOrderList.tsx
 'use client';
 
-import styles from '@/styles/myPage/cancelReturnExchange.module.css';
+import styles from '@/styles/myPage/orderDelivery.module.css';
+import buttonStyles from '@/styles/components/button.module.css';
 
-export default function WebOrderList() {
+export default function WebMembershipSection() {
   return (
-    <section className={styles.web_order_delivery_view_wrapper}>
-      <h2 className={styles.web_order_delivery_view_tit}>주문목록/배송조회 내역 총 0 건</h2>
-      <div className={styles.web_order_delivery_view_label_wrapper}>
-        <p className={styles.web_order_delivery_view_label}>날짜/주문번호</p>
-        <p className={styles.web_order_delivery_view_label}>상품명/옵션</p>
-        <p className={styles.web_order_delivery_view_label}>상품금액</p>
-        <p className={styles.web_order_delivery_view_label}>진행상태</p>
-        <p className={styles.web_order_delivery_view_label}>접수</p>
+    <section className={styles.web_order_delivery_membership_wrapper}>
+      <div className={styles.web_order_delivery_membership}>
+        <p className={styles.web_order_delivery_membership_name}>장유하님의</p>
+        {/* 버튼 컴포넌트 대체 전 임시 버튼 */}
+        <button className={buttonStyles.inquiry_button}>조회</button>
       </div>
-      <ul className={styles.web_order_delivery_view_list_wrapper}>
-        <li className={styles.web_order_delivery_view_list}>조회내역이 없습니다.</li>
-      </ul>
-      <hr className={styles.web_order_delivery_view_list_divider} />
+
+      <hr className={styles.web_order_delivery_membership_divider} />
+
+      <div className={styles.web_order_delivery_stamp}>
+        <p className={styles.web_order_delivery_label}>스탬프</p>
+        <p className={styles.web_order_delivery_count}>0 개</p>
+      </div>
+
+      <hr className={styles.web_order_delivery_membership_divider} />
+
+      <div className={styles.web_order_delivery_membership_point}>
+        <p className={styles.web_order_delivery_membership_label}>통합 멤버십 포인트</p>
+        <p className={styles.web_order_delivery_membership_point_count}>1,000 p</p>
+        <p className={styles.web_order_delivery_membership_point_info}>
+          상품 구매 후 구매 확정 시 직접 반영 됩니다.
+        </p>
+      </div>
     </section>
   );
 }
