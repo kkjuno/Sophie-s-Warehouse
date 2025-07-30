462fc66df5d9bd10325703f04f6672c36b65a500
bosshamster69 <c4110599@gmail.com>
2025-07-30 17:46:01 +0900

    ♻️ Refactor: css.module 관련 오류 리펙토링

--- src/app/recentPage/mobile/product.tsx
+++ src/app/recentPage/mobile/product.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import styles from '@/styles/recentPage/mobile/recentProduct.module.css';
 
-import { Product } from '@/types/product';
+import { Product } from '@/types/Product';
 
 interface MobileRecentProductsProps {
   initialProducts: Product[];
