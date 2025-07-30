356d1a8c6c8d43dfe326dc08631a5d74dd16414d
bosshamster69 <c4110599@gmail.com>
2025-07-30 19:49:35 +0900

    🔧 Fix: types 파일 이름 수정

--- src/types/index.ts
+++ src/types/index.ts
@@ -16,3 +16,5 @@ export * from './user';
 export * from './post';
 export * from './api';
 export * from './file';
+
+console.log('.');
--- src/types/product.ts
+++ src/types/product.ts
@@ -22,3 +22,5 @@ export interface Filter {
   emptyIcon: string;
   filledIcon: string;
 }
+
+console.log('.');
