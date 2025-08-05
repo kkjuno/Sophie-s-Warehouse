358830fb9b2b60eb49eed57c97230d54c4f51b1e
bosshamster69 <c4110599@gmail.com>
2025-08-05 16:59:13 +0900

    🔧 Fix: 3차 배포를 위한 오류 수정

--- src/app/mainPage/sections/productSection.tsx
+++ src/app/mainPage/sections/productSection.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useRef, useState, useEffect, JSX } from 'react';
+import React, { useState, useEffect, JSX } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { productFetch } from '@/app/fetch/product';
