53a0817ec0d30a4bf4e18340883702b7af6bfc81
jinseob92 <elso1234@naver.com>
2025-08-07 10:33:54 +0900

    ♻️ refactor : 이미지 경로 수정

--- src/app/mainPage/sections/productSection.tsx
+++ src/app/mainPage/sections/productSection.tsx
@@ -210,7 +210,7 @@ export default function ProductSection(): JSX.Element {
                         <div className={styles.product_image}>
                           {product.mainImages?.[0]?.path && (
                             <Image
-                              src={`/${product.mainImages[0].path}`}
+                              src={product.mainImages[0].path}
                               alt={product.name}
                               width={200}
                               height={200}
